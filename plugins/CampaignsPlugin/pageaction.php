5bef2c6559e89d99ade822e3807c436a3d525738
Duncan Cameron <phplist@dcameron.me.uk>
2017-03-04 18:04:00 +0000

    New page to handle core phplist help dialog

---
+++ plugins/CampaignsPlugin/pageaction.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CampaignsPlugin for phplist.
+ * 
+ * This file is a part of CampaignsPlugin.
+ *
+ * This plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @category  phplist
+ *
+ * @author    Duncan Cameron
+ * @copyright 2014-2017 Duncan Cameron
+ * @license   http://www.gnu.org/licenses/gpl.html GNU General Public License, Version 3
+ */
+
+/**
+ * This page and class are used to display the phplist help dialog.
+ */
+class CampaignsPlugin_PageactionController extends CommonPlugin_Controller
+{
+}
+
+$controller = new CampaignsPlugin_PageactionController();
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+$controller->run($action);
